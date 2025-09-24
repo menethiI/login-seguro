5bdc0aedde0db2afc46bd3f7c98b686a545a9b4e
ManuelaRiosSilva <86746168+menethiI@users.noreply.github.com>
2025-09-24 10:53:24 -0300

    Utilizando as anotações @Getter e @Setter do Lombok

--- src/main/java/br/com/app/login_seguro/dto/UserDTO.java
+++ src/main/java/br/com/app/login_seguro/dto/UserDTO.java
@@ -15,29 +15,4 @@ public class UserDTO {
 
     @NotBlank
     private String senha;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
 }
--- src/main/java/br/com/app/login_seguro/model/User.java
+++ src/main/java/br/com/app/login_seguro/model/User.java
@@ -2,11 +2,15 @@ package br.com.app.login_seguro.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Getter
+@Setter
 @Document(collection = "users")
-
-
 public class User {
 
     @Id
@@ -19,37 +23,5 @@ public class User {
     private String senha;
     
     private Set<String> roles;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public Set<String> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<String> roles) {
-		this.roles = roles;
-	}
     
 }
